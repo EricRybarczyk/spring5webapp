8dec0c701242129a439382ead2ad6603e956d428
Eric Rybarczyk <EricRybarczyk@gmail.com>
2020-04-27 19:33:24 -0400

    Added basic Controllers and Thymeleaf Views for the app

---
+++ src/main/java/dev/ericrybarczyk/spring5webapp/controllers/AuthorsController.java
@@ -0,0 +1,24 @@
+package dev.ericrybarczyk.spring5webapp.controllers;
+
+import dev.ericrybarczyk.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorsController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorsController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
